db6e37147c5d9f9132cae1dfdbb05985e636147f
Inaki Villar <inaki.seri@gmail.com>
2024-06-28 06:58:02 -0700

    updating test

--- library/src/test/kotlin/io/github/cdsap/comparescans/collector/TaskMetricsCollectorTest.kt
+++ library/src/test/kotlin/io/github/cdsap/comparescans/collector/TaskMetricsCollectorTest.kt
@@ -14,11 +14,24 @@ class TaskMetricsCollectorTest {
         val secondBuildTasks = secondBuild.taskExecution
 
         val metric =
-            TaskMetricsCollector().metricsTasks(firstBuildTasks, secondBuildTasks).first { it.name == ":app:preBuild" }
+            TaskMetricsCollector().metricsTasks(firstBuildTasks, secondBuildTasks)
+                .first { it.name == ":app:preBuild" && it.type == TypeMetric.Duration }
         val firstDuration = firstBuild.taskExecution.first { it.taskPath == ":app:preBuild" }.duration
         val secondDuration = secondBuild.taskExecution.first { it.taskPath == ":app:preBuild" }.duration
         assert(metric.entity == Entity.Task)
         assert(metric.type == TypeMetric.Duration)
         assert(firstDuration == metric.firstBuild && secondDuration == metric.secondBuild)
+        val metricCache =
+            TaskMetricsCollector().metricsTasks(firstBuildTasks, secondBuildTasks)
+                .first { it.name == ":core:database:packageDemoDebugResources" && it.type == TypeMetric.CacheSize }
+
+        val firstCache =
+            firstBuild.taskExecution.first { it.taskPath == ":core:database:packageDemoDebugResources" }.cacheArtifactSize
+        val secondCache =
+            secondBuild.taskExecution.first { it.taskPath == ":core:database:packageDemoDebugResources" }.cacheArtifactSize
+
+        assert(metricCache.entity == Entity.Task)
+        assert(metricCache.type == TypeMetric.CacheSize)
+        assert(firstCache == metricCache.firstBuild && secondCache == metricCache.secondBuild)
     }
 }
